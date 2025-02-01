780ec6a13d8c1062980cb03b7a7cdd6783ad0f7c
yoursrealkiran <kiranrajofficial.de@yahoo.com>
2025-02-01 14:15:41 +0100

    Initial commit

---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kiranraj/robot_ws/src/my_robot_description
+# Build directory: /home/kiranraj/robot_ws/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ install/my_robot_description/share/my_robot_description/local_setup.sh
@@ -0,0 +1,1 @@
+/home/kiranraj/robot_ws/build/my_robot_description/ament_cmake_environment_hooks/local_setup.sh
